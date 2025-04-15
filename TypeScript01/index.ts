f87edfd2b8b7b4a6e830b64d1a492cccfa5adc8b
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-15 22:12:33 +0900

    Study: TypeScript Practice

---
+++ TypeScript01/index.ts
@@ -0,0 +1,16 @@
+const myName: string = 'Donghyun';
+const myAge: number = 20;
+
+const result: string = myName + myAge;
+
+console.log(result);
+
+const region: string[] = ['서울', '부산', '인천', '대구', '대전', '울산', '광주'];
+
+const what: { name: string, age: number, isMen: boolean } = {
+    name: '김동현',
+    age: 20,
+    isMen: true
+};
+
+// 딱히 별거 없나..?
